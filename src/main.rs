1212cc7c24aa04050878ac041ac668183ea50c87
Denis Kolodin <deniskolodin@gmail.com>
2019-08-03 13:05:01 +0900

    Respawn a process with a new configuration

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,24 @@ mod settings;
 use crate::settings::{BinSettings, Name, Settings};
 use colored::*;
 use failure::Error;
+use futures::channel::oneshot;
 use futures::compat::{Future01CompatExt, Stream01CompatExt};
-use futures::{select, StreamExt};
+use futures::{select, FutureExt, StreamExt};
 use futures_legacy::prelude::*;
 use runtime::task::JoinHandle;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::env;
 use std::io::BufReader;
-use std::process::{Command, Stdio};
+use std::process::{Command, ExitStatus, Stdio};
 use tokio_process::{Child, ChildStdout, CommandExt};
 use tokio_signal::unix::{Signal, SIGHUP, SIGINT};
 
-async fn run_command(name: Name, bin: BinSettings) -> Result<(), Error> {
+async fn run_command(
+    name: Name,
+    bin: BinSettings,
+    killer: oneshot::Receiver<()>,
+) -> Result<ExitStatus, Error> {
     log::info!("Starting '{}': {}", name, bin.path);
     let mut cmd = Command::new(bin.path);
     let mut filtered_env: HashMap<String, String> = env::vars()
@@ -33,46 +38,73 @@ async fn run_command(name: Name, bin: BinSettings) -> Result<(), Error> {
         Ok(mut child) => {
             log::debug!("Started: '{}'", name);
             if let Some(stderr) = child.stderr().take() {
-                let mut lines = tokio_io::io::lines(BufReader::new(stderr)).compat();
-                runtime::spawn(child.compat());
-                while let Some(line) = lines.next().await {
-                    match line {
-                        Ok(line) => {
-                            println!("{} | {}", name.green(), line);
+                let mut lines = tokio_io::io::lines(BufReader::new(stderr)).compat().fuse();
+                let mut killer = killer.fuse();
+                //runtime::spawn(child.compat());
+                loop {
+                    select! {
+                        line = lines.next() => {
+                            match line {
+                                Some(Ok(line)) => {
+                                    println!("{} | {}", name.green(), line);
+                                }
+                                Some(Err(err)) => {
+                                    log::warn!("Can't read line from stderr of '{}': {}", name, err);
+                                }
+                                None => {
+                                    break;
+                                }
+                            }
                         }
-                        Err(err) => {
-                            log::warn!("Can't read line from stderr of '{}': {}", name, err);
+                        kill = killer => {
+                            // TODO: Send signals instead
+                            child.kill();
                         }
                     }
                 }
             } else {
                 log::warn!("Can't get a stderr stream of '{}'", name);
             }
+            child.from_err().compat().await
         }
         Err(err) => {
             log::error!("Can't start '{}': {}", name, err);
+            Err(Error::from(err))
         }
     }
-    Ok(())
 }
 
 /// This struct holds `JoinHandle` of a spawned routine that
 /// reprints output and contains a channel to send management commands
 /// to a process. But maybe use signals to end them?
 struct RunContext {
-    handle: JoinHandle<Result<(), Error>>,
+    handle: JoinHandle<Result<ExitStatus, Error>>,
+    name: Name,
     bin: BinSettings,
+    killer: Option<oneshot::Sender<()>>,
 }
 
 impl RunContext {
     fn start(name: Name, bin: BinSettings) -> Self {
-        let handle = runtime::spawn(run_command(name, bin.clone()));
-        Self { handle, bin }
+        let (tx, rx) = oneshot::channel();
+        let handle = runtime::spawn(run_command(name.clone(), bin.clone(), rx));
+        Self {
+            handle,
+            name,
+            bin,
+            killer: Some(tx),
+        }
     }
 
     async fn end(&mut self) -> Result<(), Error> {
-        // TODO: Send termination signal to a process
-        (&mut self.handle).await;
+        if let Some(killer) = self.killer.take() {
+            if let Err(_) = killer.send(()) {
+                log::error!("Can't send termination signal to '{}'", self.name);
+            }
+            (&mut self.handle).await;
+        } else {
+            log::error!("Attempt to call end twice for '{}'", self.name);
+        }
         Ok(())
     }
 }
@@ -97,10 +129,8 @@ impl Supervisor {
                     if context.bin != bin {
                         log::debug!("Restarting process '{}'...", name);
                         context.end().await;
-                    // TODO:
-                    // 1. Kill `context`
-                    // 2. Spawn new `RunContext`
-                    // 3. Replace new with old
+                        let new_context = RunContext::start(name.clone(), bin);
+                        *context = new_context;
                     } else {
                         log::debug!("Process '{}' already started", name);
                     }
